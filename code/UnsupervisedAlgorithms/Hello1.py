6fd5a45348f24bca0697cf86b64aab4ebeb7f14a
Kolappan <kolappan@gmail.com>
2023-09-30 22:20:16 +1000

    ImplementingKMeans

--- code/UnsupervisedAlgorithms/Hello1.py
+++
@@ -1,9 +0,0 @@
-from kp2 import Student
-
-Student1 = Student("Kols", "EEE", 2)
-Student2 = Student(False,2,2)
-
-print (Student1.name)
-
-print(Student2.name)
-
---
+++ code/UnsupervisedAlgorithms/ImplementingKMeans.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+
+
+def find_closest_centroids (X, centriods):
+    """ Computes Centroid memberships for every example
+    Args: 
+
+        X(ndarray): (m,n) Input Values
+        centriods (ndarray): (K,n) Centriods
+
+    Returns:
+        idx(array_like): (m,) closest centriods
+    
+    """
+
+    print ("X.shape[0]", X.shape[0])
+
+    # Set K
+    K = centriods.shape[0]
+
+    # You need to return the following variables correctly
+    idx = np.zeros(X.shape[0],dtype=int)
+    mindistance = 0
+
+    ### START CODE HERE ###
+    for i in range(X.shape[0]):
+        # Array to hold the distance between x[i] and each centroid[j]
+        distance=[]
+        for j in range(K):
+            norm_ij = np.linalg.norm(X[i]-centriods[j])
+            distance.append(norm_ij)
+        idx[i] = np.argmin(distance)
+
+    return idx
+
+
+
+
+
